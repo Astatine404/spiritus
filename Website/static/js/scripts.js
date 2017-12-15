fefe24a1149710e5ef04ec3921884e6aa6215c80
Saurabh Bodhe <samzor999@live.com>
2017-12-15 20:35:29 +0530

    Progress Bar

--- Website/static/js/scripts.js
+++ Website/static/js/scripts.js
@@ -9,4 +9,22 @@ function implement_fullpage()
 $(document).ready(function() {
 	implement_fullpage();
 	// cust_controls();
+	$('.progress').hide();
 });
+
+function progress_bar()
+{
+	$('.progress').show();
+	var current_progress = 0;
+  	var interval = setInterval(function() {
+      current_progress += parseInt(10 * Math.random());
+      if (current_progress >= 100)
+      	current_progress = 100;
+      $("#dynamic")
+      .css("width", current_progress + "%")
+      .attr("aria-valuenow", current_progress)
+      .text(current_progress + "% Complete");
+      if (current_progress >= 100)
+          clearInterval(interval);
+  }, 1000);
+}
--- Website/views.py
+++ Website/views.py
@@ -57,3 +57,44 @@ def index(request):
 		vid = None
 		
 		return render(request, 'Website/index.html', {'form': form, 'vid': vid})
+
+
+def process(request):
+	if request.method == 'POST':
+
+		root = settings.MEDIA_ROOT
+		myfile = request.FILES['myfile']
+		
+		vvid = Music()
+		vvid.video = myfile
+		vvid.save()
+ 
+		vids = root + str(vvid.video)
+		auds = root + "/mozz.mp3"
+		resu = "final_" + str(vvid.video)
+		resl = root + resu
+
+		ovid = Music()
+		ovid.video = resu
+		ovid.save()
+
+		videoclip = VideoFileClip(vids)
+		background_music = mpe.AudioFileClip(auds)
+
+		if videoclip.duration < background_music.duration:
+			clipped_back = background_music.set_end(videoclip.duration)
+		else:
+			clipped_back = background_music
+
+		vvid.delete()
+
+		new_clip = videoclip.set_audio(clipped_back)
+		new_clip.write_videofile(resl)
+
+		if myfile:
+			resu = "/media/" + resu
+			return render(request, 'Website/index.html', {'form': form, 'vid': myfile, 'resu': resu})
+		else:
+			return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+	else:
+		print ("POST Error!")
